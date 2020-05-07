ba51b1a3ca887008e94614003e6a96616dff36f3
lynnelyin <1440353878@qq.com>
2020-05-07 17:21:33 +0800

    init router

---
+++ blog-node/src/router/blog.js
@@ -0,0 +1,31 @@
+const handleBlogRouter = (req, res) => {
+  const method = req.method
+
+  if (method == 'GET' && req.path == '/api/blog/list') {
+    return {
+      msg: '博客列表接口'
+    }
+  }
+  if (method == 'GET' && req.path == '/api/blog/detail') {
+    return {
+      msg: ' 博客详情接口'
+    }
+  }
+  if (method == 'POST' && req.path == '/api/blog/new') {
+    return {
+      msg: '新建博客接口'
+    }
+  }
+  if (method == 'POST' && req.path == '/api/blog/update') {
+    return {
+      msg: '更新博客接口'
+    }
+  }
+  if (method == 'POST' && req.path == '/api/blog/del') {
+    return {
+      msg: '删除博客接口'
+    }
+  }
+}
+
+module.exports = handleBlogRouter
