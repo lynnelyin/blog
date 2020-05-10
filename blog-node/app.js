3cb2e3794af9e0225e42ed39ccb4c89199153b02
lynnelyin <1440353878@qq.com>
2020-05-10 15:31:04 +0800

    404 bugfix

--- blog-node/app.js
+++ blog-node/app.js
@@ -2,6 +2,30 @@ const handleBlogRouter = require('./src/router/blog')
 const handlerUserRouter = require('./src/router/user')
 const querystring = require('querystring')
 
+const getPostData = (req) => {
+  return new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({})
+      return
+    }
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(JSON.parse(postData))
+    })
+  })
+}
+ 
 const serverHandler = (req, res) => {
   // 设置返回格式为 JSON
   res.setHeader('Content-type', 'application/json')
@@ -13,40 +37,33 @@ const serverHandler = (req, res) => {
   // 解析 query
   req.query = querystring.parse(url.split('?')[1])
 
-  // 处理 blog 路由
-  const blogResult = handleBlogRouter(req, res)
-  if (blogResult) {
-    blogResult.then(blogData => {
-      res.end(JSON.stringify(blogData))
-    })
-    return
-  }
-  /*
-  const blogData = handleBlogRouter(req, res)
-  if (blogData) {
-    res.end(JSON.stringify(blogData))
-    return
-  }
-  */ 
-  // 处理 user 路由
-  const userResult = handlerUserRouter(req, res)
-  if (userResult) {
-    userResult.then(userData => {
-      res.end(JSON.stringify(userData))
-    })
-    return
-  }
-  /*
-  const userData = handlerUserRouter(req, res)
-  if (userData) {
-    res.end(JSON.stringify(userData))
-    return
-  }
-  */ 
-  // 未命中路由，返回 404
-  res.writeHead(404, {"Content-type": "text/plain"})
-  res.write("404 Not Found\n")
-  res.end()
+  // 处理 post data
+  getPostData(req).then(postData => {
+    req.body = postData
+
+    // 处理 blog 路由
+    const blogResult = handleBlogRouter(req, res)
+    if (blogResult) {
+      blogResult.then(blogData => {
+        res.end(JSON.stringify(blogData))
+      })
+      return
+    }
+  
+    // 处理 user 路由
+    const userResult = handlerUserRouter(req, res)
+    if (userResult) {
+      userResult.then(userData => {
+        res.end(JSON.stringify(userData))
+      })
+      return
+    }
+
+    // 未命中路由，返回 404
+    res.writeHead(404, {"Content-type": "text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
+  })
 }
 
 module.exports = serverHandler
