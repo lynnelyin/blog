71f0b68036624f9c107b748197be7af633b2fc0a
lynnelyin <1440353878@qq.com>
2020-05-19 16:59:29 +0800

    express router

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -3,19 +3,20 @@ const router = express.Router()
 
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
-router.get('/list', function(req, res, next) {
+router.get('/list', (req, res, next) => {
   let author = req.query.author || ''
   const keyword = req.query.keyword || ''
   
   if (req.query.isadmin) {
-    /*
     // 管理员界面
-    const loginCheckResult = loginCheck(req)
-    if (loginCheckResult) {
-      return loginCheckResult
+    if (req.session.username == null) {
+      res.json(
+        new ErrorModel('未登录')
+      )
+      return
     }
-    */
     // 强制查询自己的博客
     author = req.session.username
   }
@@ -23,6 +24,56 @@ router.get('/list', function(req, res, next) {
   const result = getList(author, keyword)
   return result.then(listData => {
     res.json(new SuccessModel(listData))
+  })
+})
+
+router.get('/detail', (req, res, next) => {
+  const result = getDetail(req.query.id)
+  return result.then(data => {
+    res.json(
+      new SuccessModel(data)
+    )
+  })
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+  req.body.author = req.session.username
+  const result = newBlog(req.body)
+  return result.then(data => {
+    res.json(
+      new SuccessModel(data)
+    )
+  })
+})
+
+router.post('/update', loginCheck, (req, res, next) => {
+  const result = updateBlog(req.query.id, req.body)
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel()
+      )
+    } else {
+      res.json(
+        new ErrorModel('更新博客失败')
+      )
+    }
+  })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+  const author = req.session.username
+  const result = delBlog(req.query.id, author)
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel()
+      )
+    } else {
+      res.json(
+        new ErrorModel('删除博客失败')
+      )
+    }
   })
 })
 
